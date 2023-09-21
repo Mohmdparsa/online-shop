1d38ad113d7ea2117ee58eb4e5696e536396a676
Mohammad Parsapasand <mohmd.pars4@gmail.com>
2023-09-21 12:37:53 +0430

    Feat(index.jsx): create index.jsx to import mainComponent there

--- src/index.js
+++ src/index.js
@@ -2,40 +2,43 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter , Routes , Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./headerComponent/Login";
-import SignIn from "./headerComponent/SignIn"
-import GamingAccessories from "./mainComponent/GILinkComponent/GamingAccessories"
-import Electronics from "./mainComponent/GILinkComponent/Electronics"
-import HomeAndKitchen from "./mainComponent/GILinkComponent/Home&Kitchen"
-import HealthAndPersonalCare from "./mainComponent/GILinkComponent/Health&PersonalCare"
-import ForYourFitnessNeeds from "./mainComponent/GILinkComponent/ForYourFitnessNeeds"
-import FindYourIdealTV from "./mainComponent/GILinkComponent/FindYourIdealTV"
-import ShopLaptopsAndTablets from "./mainComponent/GILinkComponent/ShopLaptops&Tablets"
-import Smartwatches from "./mainComponent/GILinkComponent/smartwatches"
-
+import SignIn from "./headerComponent/SignIn";
+import {
+  GamingAccessories,
+  Electronics,
+  HomeAndKitchen,
+  HealthAndPersonalCare,
+  ForYourFitnessNeeds,
+  FindYourIdealTV,
+  ShopLaptopsAndTablets,
+  Smartwatches,
+} from "./mainComponent";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App/>}/>
-      <Route path="/Login" element={<Login/>}/>
-      <Route path="/SignIn" element={<SignIn/>}/>
-      <Route path="/GamingAccessories" element={<GamingAccessories/>}/>
-      <Route path="/Electronics" element={<Electronics/>}/>
-      <Route path="/HomeAndKitchen" element={<HomeAndKitchen/>}/>
-      <Route path="/HealthAndPersonalCare" element={<HealthAndPersonalCare/>}/>
-      <Route  path="/ForYourFitnessNeeds" element={<ForYourFitnessNeeds/>}/>
-      <Route  path="/FindYourIdealTV" element={<FindYourIdealTV/>}/>
-      <Route  path="/ShopLaptopsAndTablets" element={<ShopLaptopsAndTablets/>}/>
-      <Route  path="/Smartwatches" element={<Smartwatches/>}/>
-
-
-
-     
-    </Routes>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/SignIn" element={<SignIn />} />
+        <Route path="/GamingAccessories" element={<GamingAccessories />} />
+        <Route path="/Electronics" element={<Electronics />} />
+        <Route path="/HomeAndKitchen" element={<HomeAndKitchen />} />
+        <Route
+          path="/HealthAndPersonalCare"
+          element={<HealthAndPersonalCare />}
+        />
+        <Route path="/ForYourFitnessNeeds" element={<ForYourFitnessNeeds />} />
+        <Route path="/FindYourIdealTV" element={<FindYourIdealTV />} />
+        <Route
+          path="/ShopLaptopsAndTablets"
+          element={<ShopLaptopsAndTablets />}
+        />
+        <Route path="/Smartwatches" element={<Smartwatches />} />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
---
+++ src/mainComponent/index.jsx
@@ -0,0 +1,10 @@
+export {default as GamingAccessories} from "./GILinkComponent/GamingAccessories"
+export {default as Electronics} from  "./GILinkComponent/Electronics"
+export {default as HomeAndKitchen} from "./GILinkComponent/Home&Kitchen"
+export {default as HealthAndPersonalCare} from "./GILinkComponent/Health&PersonalCare"
+export {default as  ForYourFitnessNeeds} from "./GILinkComponent/ForYourFitnessNeeds"
+export {default as  FindYourIdealTV} from "./GILinkComponent/FindYourIdealTV"
+export {default as ShopLaptopsAndTablets} from "./GILinkComponent/ShopLaptops&Tablets"
+export {default as Smartwatches} from "./GILinkComponent/smartwatches"
+
+ 
