1bd0381d634165db85dd5c948fec7132ea86a384
Mohammad Parsapasand <mohmd.pars4@gmail.com>
2023-10-02 11:50:36 +0330

    refactor(GAItems.jsx): make some basic change in GAItems

--- src/mainComponent/GILinkComponent/GamingAccessoriesItemsComponent/GAItems.jsx
+++ src/mainComponent/GILinkComponent/GamingAccessoriesItemsComponent/GAItems.jsx
@@ -1,39 +1,43 @@
 import styles from "./GAItems.module.css";
 import { useState } from "react";
 import Spinner from "../../Spinner";
-const GAItems = ({loading , GAItems}) => {
+const GAItems = ({ loading, GAItems }) => {
   const [count, setCount] = useState(0);
 
   const Increase = () => {
     setCount(count + 1);
   };
   const decrease = () => {
-    if(count>0){
-      setCount(count - 1);  
+    if (count > 0) {
+      setCount(count - 1);
     }
-    
   };
 
   return (
     <>
-    
-    
-           <div className={styles.GAItemsContainer}>
+      <div className={styles.GAItemsContainer}>
         <div className={styles.GAItemsImg}>
           <img src={GAItems.photo} alt="" />
         </div>
-        <div>
+        <div className={styles.GAItemsDescription}>
           <h3 className={styles.GAItemsName}>{GAItems.name}</h3>
-          <p className={styles.GAItemsDesc}>description:{GAItems.desc}</p>
+          <p className={styles.GAItemsDesc}>{GAItems.desc}</p>
         </div>
         <div className={styles.GAItemsLine}></div>
 
-        <div>
+        <div> 
+          <br />
+          <br />
+           <span className={styles.GAItemsCost}>
+            cost:<b>{GAItems.cost}</b>
+          </span>
+          <br />
+          <br />
           <button
             onClick={decrease}
             style={{ marginLeft: "45px" }}
             className={styles.GAItemsBtn}
-          >
+            >
             -
           </button>
           <span className={styles.GAItemsCount}>{count}</span>
@@ -42,17 +46,12 @@ const GAItems = ({loading , GAItems}) => {
           </button>
           <br />
           <br />
-          <br />
-          <span className={styles.GAItemsCost}>
-            cost:<b>$</b>
-          </span>
-          <br />
+        
           <button className={styles.GAItemsSubmitBtn}>
             Add to Shopping Cart
           </button>
         </div>
       </div>
-   
     </>
   );
 };
