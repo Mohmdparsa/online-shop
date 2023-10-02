a0e3312a59317baa719bbe171bddb4c53941f226
Mohammad Parsapasand <mohmd.pars4@gmail.com>
2023-10-02 17:49:08 +0330

    Feat(Login.jsx): remake login component

--- src/headerComponent/Login.jsx
+++ src/headerComponent/Login.jsx
@@ -1,29 +1,55 @@
-import styles from "./Login.module.css"
+import { NavLink } from "react-router-dom";
+import styles from "./Login.module.css";
 const Login = () => {
   return (
     <div>
       <div className={styles.loginContainer}>
         <table className={styles.loginTable}>
           <tr>
-            <td>User Name</td>
+            <h2 className={styles.loginTitle}>Login</h2>
           </tr>
+
           <tr>
             <input
-            className={styles.loginInput}
+              className={styles.loginInput}
               type="text"
               placeholder="User name"
             />
           </tr>
+          <br />
+
           <tr>
-            <caption>Pasword</caption>
-          </tr>
-          <tr>
-        <input className={styles.loginInput} type="text" placeholder="Pasword" />
+            <input
+              className={styles.loginInput}
+              type="text"
+              placeholder="Pasword"
+            />
           </tr>
           <br />
           <tr>
-            <button className={styles.loginSubmit}>Sumbit</button>
+            <span className={styles.loginForgotPassword}>Forgot password?</span>
           </tr>
+          <tr>
+            <button className={styles.loginSubmit}>Login</button>
+          </tr>
+          <tr>
+            <p style={{ marginLeft: "70px" }}>
+              {" "}
+              dont have an account?
+              <NavLink
+                to="/signIn"
+                style={({ isActive }) => {
+                  return { color: isActive ? "red" : "" };
+                }}
+              >
+                SignIn
+              </NavLink>
+            </p>
+          </tr>
+          <fieldset className={styles.loginFieldset}>
+            <legend>Or</legend>
+            <div className={styles.LoginGoogleLogin}><img className={styles.LoginGoogleImg} src={require("../assets/GoogleLogo/googleLogo.jpg")} alt="" /> Login with Google</div>
+          </fieldset>
         </table>
       </div>
     </div>
