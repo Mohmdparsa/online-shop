932e169d0391ab815653bc38606bce9b6dc75bd2
Mohammad Parsapasand <mohmd.pars4@gmail.com>
2023-12-02 23:12:36 +0330

    Feat(SignIn.jsx) : completely overhaul code

--- src/headerComponent/SignIn.jsx
+++ src/headerComponent/SignIn.jsx
@@ -1,73 +1,71 @@
 import styles from "./SignIn.module.css";
 const SignIn = () => {
   return (
-    <div>
-      <div className={styles.mainDiv}>
-        <h2 className={styles.registerTitle}> Registeration</h2>
-        <hr />
-        <br />
-        <div className={styles.total}>
-          <label for="">First name:</label>
-          <br />
-          <input
-            className={styles.input}
-            type="text"
-            name="First-Name"
-            placeholder="Firstname"
-          ></input>
-          <br />
-          <br />
-          <label for="">Last name:</label>
-          <br />
-          <input
-            className={styles.input}
-            type="text"
-            name="Last-Name"
-            placeholder="Lastname"
-          ></input>
-          <br />
-          <br />
-          <label for="">Phone:</label>
-          <br />
-          <input
-            className={styles.phone}
-            type="text"
-            name="phone"
-            placeholder="09xxxxxxxxx"
-          ></input>
-          <br />
-          <br />
-          <label for="">Email:</label>
-          <br />
-          <input
-            className={styles.input}
-            type="text"
-            name="email"
-            placeholder="Email"
-          ></input>
-          <br />
-          <br />
-          <label for="">Password</label>
-          <br />
-          <input
-            className={styles.input}
-            type="password"
-            placeholder="Password"
-          ></input>
-          <br />
-          <br />
-          <label for="">Re-type-Password</label>
-          <br />
-          <input
-            className={styles.input}
-            type="password"
-            placeholder="Re-type-Password"
-          ></input>
-          <br />
-          <br />
-          <button className={styles.registerBtn} type="submit">
-            Register
-          </button>
+    <div className={styles.formContainer}>
+      <div className={styles.formImage}></div>
+      <div className={styles.mainForm}>
+        <div className={styles.mainFormSubset}>
+          <table className={styles.formTable}>
+            <tr>
+              <label className={styles.inputLabel}>FirstName:</label>
+              <input
+                type="text"
+                name="firstName"
+                className={`${styles.formInputs} , ${styles.formFirstName} `}
+                placeholder="first name"
+              />
+            </tr>
+
+            <tr>
+              <label className={styles.inputLabel}>SecondName:</label>
+              <input
+                type="text"
+                name="secondName"
+                className={`${styles.formInputs} , ${styles.formSecondName} `}
+                placeholder="second name"
+              />
+            </tr>
+            <tr>
+              <label className={styles.inputLabel}>Phone:</label>
+              <input
+                type="text"
+                name="phone"
+                className={`${styles.formInputs} , ${styles.formPhone} `}
+                placeholder="phone number"
+              />
+            </tr>
+            <tr>
+              <label className={styles.inputLabel}>Email:</label>
+              <input
+                type="text"
+                name="email"
+                className={`${styles.formInputs} , ${styles.formEmail} `}
+                placeholder="email@email.com"
+              />
+            </tr>
+            <tr>
+              <label className={styles.inputLabel}>Password:</label>
+              <input
+                type="text"
+                name="password"
+                className={`${styles.formInputs} , ${styles.formPassword} `}
+                placeholder="password"
+              />
+            </tr>
+            <tr>
+              <label className={styles.inputLabel}>Re-Type-Password:</label>
+              <input
+                type="text"
+                name="re-type-password"
+                className={`${styles.formInputs} , ${styles.formReTypePassword} `}
+                placeholder="re-type-password"
+              />
+            </tr>
+            <tr>
+              <button className={styles.cancelBtn}>Cancel</button>
+              <button className={styles.submitBtn}>Submit</button>
+            </tr>
+          </table>
         </div>
       </div>
     </div>
