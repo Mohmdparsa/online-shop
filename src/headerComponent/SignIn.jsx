2f64c8f0a2fba01a464326fadfca7a18ab759b03
Mohammad Parsapasand <mohmd.pars4@gmail.com>
2023-12-03 18:15:11 +0330

    Feat(SignIn.jsx): integrated Formik for signIn form handling

--- src/headerComponent/SignIn.jsx
+++ src/headerComponent/SignIn.jsx
@@ -1,71 +1,97 @@
+import { FormSchema } from "../validations/FormValidation";
 import styles from "./SignIn.module.css";
+import {Formik , Form , Field , ErrorMessage} from "formik"
 const SignIn = () => {
   return (
     <div className={styles.formContainer}>
       <div className={styles.formImage}></div>
       <div className={styles.mainForm}>
         <div className={styles.mainFormSubset}>
+          <Formik 
+          initialValues={{
+            firstName : "",
+            secondName : "",
+            phone : "",
+            email : "",
+            password : "",
+            reTypePassword:""
+
+          }}
+          validationSchema={FormSchema}
+          onSubmit={(values)=>{
+            console.log(values)
+          }}
+          >
+            <Form>
           <table className={styles.formTable}>
             <tr>
               <label className={styles.inputLabel}>FirstName:</label>
-              <input
+              <Field
                 type="text"
                 name="firstName"
                 className={`${styles.formInputs} , ${styles.formFirstName} `}
                 placeholder="first name"
               />
+              <ErrorMessage name="firstName" render={(msg) =>(<p style={{display:"inline" , color:"red"}}>{msg}</p>)}/>
             </tr>
 
             <tr>
               <label className={styles.inputLabel}>SecondName:</label>
-              <input
+              <Field
                 type="text"
                 name="secondName"
                 className={`${styles.formInputs} , ${styles.formSecondName} `}
                 placeholder="second name"
               />
+                            <ErrorMessage name="secondName" render={(msg) =>(<p style={{display:"inline" , color:"red"}}>{msg}</p>)}/>
             </tr>
             <tr>
               <label className={styles.inputLabel}>Phone:</label>
-              <input
+              <Field
                 type="text"
                 name="phone"
                 className={`${styles.formInputs} , ${styles.formPhone} `}
                 placeholder="phone number"
               />
+              <ErrorMessage name="phone" render={(msg) =>(<p style={{display:"inline" , color:"red"}}>{msg}</p>)}/>
             </tr>
             <tr>
               <label className={styles.inputLabel}>Email:</label>
-              <input
+              <Field
                 type="text"
                 name="email"
                 className={`${styles.formInputs} , ${styles.formEmail} `}
                 placeholder="email@email.com"
               />
+                <ErrorMessage name="email" render={(msg) =>(<p style={{display:"inline" , color:"red"}}>{msg}</p>)}/>
             </tr>
             <tr>
               <label className={styles.inputLabel}>Password:</label>
-              <input
+              <Field
                 type="text"
                 name="password"
                 className={`${styles.formInputs} , ${styles.formPassword} `}
                 placeholder="password"
               />
+                <ErrorMessage name="password" render={(msg) =>(<p style={{display:"inline" , color:"red"}}>{msg}</p>)}/>
             </tr>
             <tr>
               <label className={styles.inputLabel}>Re-Type-Password:</label>
-              <input
+              <Field
                 type="text"
-                name="re-type-password"
+                name="reTypepassword"
                 className={`${styles.formInputs} , ${styles.formReTypePassword} `}
                 placeholder="re-type-password"
               />
+                <ErrorMessage name="reTypepassword" render={(msg) =>(<p style={{display:"inline" , color:"red"}}>{msg}</p>)}/>
             </tr>
             <tr>
               <button className={styles.cancelBtn}>Cancel</button>
               <button className={styles.submitBtn}>Submit</button>
             </tr>
           </table>
+          </Form>
+          </Formik>
         </div>
       </div>
     </div>
