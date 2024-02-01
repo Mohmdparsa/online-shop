07275dbf59a6c188b6fa0525bd66eecbaad2709d
mohammad parsapasand <mohmd.pars4@gmail.com>
2024-02-01 01:39:41 -0800

    feat(App.js): implement rtl support for the project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import rtlPlugin from "stylis-plugin-rtl";
+import { CacheProvider } from "@emotion/react";
+import createCache from "@emotion/cache";
+import { prefixer } from "stylis";
+import { Button } from "@mui/material";
+import "./App.css";
 
-import './App.css';
-
+const theme = createTheme({
+  direction: "rtl",
+});
+const cacheRTL = createCache({
+  key: "muirtl",
+  stylisPlugins: [prefixer, rtlPlugin],
+});
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <CacheProvider value={cacheRTL}>
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <Button variant="contained">hello</Button>
+        </div>
+      </ThemeProvider>
+    </CacheProvider>
   );
 }
 
