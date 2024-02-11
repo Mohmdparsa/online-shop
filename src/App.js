63e6d71158fcbdc0e3909a2dc32e9bcbd5018e3b
mohammad parsapasand <mohmd.pars4@gmail.com>
2024-02-11 05:48:50 -0800

    chore(App.js): remove unused Button and Typography

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import rtlPlugin from "stylis-plugin-rtl";
 import { CacheProvider } from "@emotion/react";
 import createCache from "@emotion/cache";
 import { prefixer } from "stylis";
-import { Button, Typography } from "@mui/material";
 import {FirstNavbar , SecondNavbar} from "./Component/header/Header"
 import SwipeableImage from "./Component/SwipeableImage";
 import ProductsPartition from "./Component/ProductsPartition";
