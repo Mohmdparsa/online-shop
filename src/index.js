3b2b8ede7f6c93b2d87b7abe022313dea69e68c5
mohammad parsapasand <mohmd.pars4@gmail.com>
2024-02-12 10:04:16 -0800

    feat(index.js): add ThemeProvider , CreateTheme , rtlPlugin , prefixer , createCache

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-
+import { ThemeProvider} from "@mui/material/styles";
+import { CacheProvider } from "@emotion/react";
+import theme from "./layouts/Theme";
+import { prefixer } from "stylis";
+import rtlPlugin from "stylis-plugin-rtl";
+import createCache from "@emotion/cache";
 import router from "./routes/Route";
+
+const cacheRTL = createCache({
+  key: "muirtl",
+  stylisPlugins: [prefixer, rtlPlugin],
+});
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+     <CacheProvider value={cacheRTL}>
+      <ThemeProvider theme={theme}>
     <RouterProvider router={router} />
+    </ThemeProvider>
+    </CacheProvider>
   </React.StrictMode>
 );
