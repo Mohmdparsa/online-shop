92f0515cde15ef21354a9d24aecfc2f0852ff7e6
mohammad parsapasand <mohmd.pars4@gmail.com>
2024-02-12 01:55:03 -0800

    chore(index.js): import RouterProvider from rrd

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import router from "./routes/Route";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-
