e8b59aef3ce6cce90cad5d0274cadeb5b62c9a47
mohammad parsapasand <mohmd.pars4@gmail.com>
2024-02-24 02:57:04 -0800

    refactor(SignIn.jsx): implement new style and use useFormik and implement redux in our signIn form

--- src/Component/header/headerTabs/SignIn.jsx
+++ src/Component/header/headerTabs/SignIn.jsx
@@ -1,172 +1,140 @@
-import { Container, TextField, CardContent, Grid, Button } from "@mui/material";
+import ValidationSchema from "../../../validations/Schema";
 import { useFormik } from "formik";
-import validationSchema from "../../../validations/Schema";
-const SignIn = () => {
+import { TextField, Button, Grid } from "@mui/material";
+import { updateFormData } from "../../../features/FormSlice";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+const FormList = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const formik = useFormik({
     initialValues: {
-      name: "",
-      lastName: "",
+      fullname: "",
       email: "",
       password: "",
       repeatPassword: "",
     },
-    validationSchema: validationSchema,
+    validationSchema: ValidationSchema,
     onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2));
+      const { fullname } = values;
+      dispatch(updateFormData(fullname));
+      navigate("/ShoppingCart");
     },
   });
+
   return (
     <>
-      <Container>
-        <form>
-          <CardContent>
-            <Grid
-              container
-              sx={{
-                border: "1px  solid  gray",
-                borderRadius: "15px",
-                display: "flex",
-                flexDirection: "column",
-                textAlign: "center",
-                direction: "ltr",
-                bgcolor: "#d4d0c5",
-              }}
-              xs={12}
-              sm={12}
-              md={8}
-              lg={8}
-            >
-              <Grid item>
+      <Grid container style={{ marginTop: "60px", justifyContent: "center" }}>
+        <Grid
+          item
+          style={{
+            marginLeft: "30px",
+            boxShadow: "0 0 5px 5px gray",
+            borderRadius: "20px",
+          }}
+        >
+          <h1 style={{ textAlign: "center" }}>فرم ثبت نام</h1>
+          <form
+            onSubmit={formik.handleSubmit}
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              marginLeft: "20px",
+              marginRight: "20px",
+              marginBottom: "40px",
+            }}
+          >
+            <div>
+              <TextField
+                type="text"
+                label="نام و نام خانوادگی"
+                name="fullname"
+                style={{ width: "40vw", marginTop: "22px", color: "black" }}
+                value={formik.values.fullname}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                error={
+                  formik.touched.fullname && Boolean(formik.errors.fullname)
+                }
+                helperText={formik.touched.fullname && formik.errors.fullname}
+              />
+            </div>
+            <div>
+              <TextField
+                type="text"
+                label="ایمیل"
+                name="email"
+                style={{ width: "40vw", marginTop: "22px" }}
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                error={formik.touched.email && Boolean(formik.errors.email)}
+                helperText={formik.touched.email && formik.errors.email}
+              />
+            </div>
+            <div>
+              <TextField
+                type="password"
+                label="رمزعبور"
+                name="password"
+                style={{ width: "40vw", marginTop: "22px" }}
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                error={
+                  formik.touched.password && Boolean(formik.errors.password)
+                }
+                helperText={formik.touched.password && formik.errors.password}
+              />
+            </div>
+            <div>
+              <TextField
+                type="password"
+                label="تکرار رمزعبور"
+                name="repeatPassword"
+                style={{ width: "40vw", marginTop: "22px" }}
+                value={formik.values.repeatPassword}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                error={
+                  formik.touched.repeatPassword &&
+                  Boolean(formik.errors.repeatPassword)
+                }
+                helperText={
+                  formik.touched.repeatPassword && formik.errors.repeatPassword
+                }
+              />
+            </div>
+            <div>
+              <Button
+                style={{ marginTop: "30px", marginRight: "40px" }}
+                type="submit"
+                variant="contained"
+                color="primary"
+              >
                 {" "}
-                <h2>ثبت نام</h2>
-              </Grid>
-              <Grid item>
-                <TextField
-                  margin="normal"
-                  fullWidth
-                  size="small"
-                  placeholder="نام"
-                  name="name"
-                  variant="outlined"
-                  id="name"
-                  value={formik.values.name}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  error={formik.touched.name && Boolean(formik.errors.name)}
-                  helperText={formik.touched.name && formik.errors.name}
-                  sx={{
-                    width: { xs: "90%", sm: "77%" },
-                    bgcolor: "whitesmoke",
-                    color: "black",
-                  }}
-                  InputLabelProps={{ dir: "rtl" }}
-                />
-              </Grid>
-              <Grid item>
-                <TextField
-                  margin="normal"
-                  fullWidth
-                  size="small"
-                  placeholder="نام خانوادگی"
-                  name="lastName"
-                  variant="outlined"
-                  id="lastName"
-                  value={formik.values.lastName}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  error={
-                    formik.touched.lastName && Boolean(formik.errors.lastName)
-                  }
-                  helperText={formik.touched.lastName && formik.errors.lastName}
-                  sx={{
-                    width: { xs: "90%", sm: "77%" },
-                    bgcolor: "whitesmoke",
-                    color: "black",
-                  }}
-                />
-              </Grid>
-              <Grid item>
-                <TextField
-                  margin="normal"
-                  fullWidth
-                  size="small"
-                  placeholder="آدرس ایمیل"
-                  name="email"
-                  variant="outlined"
-                  id="email"
-                  value={formik.values.email}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  error={formik.touched.email && Boolean(formik.errors.email)}
-                  helperText={formik.touched.email && formik.errors.email}
-                  sx={{
-                    width: { xs: "90%", sm: "77%" },
-                    bgcolor: "whitesmoke",
-                    color: "black",
-                  }}
-                />
-              </Grid>
-              <Grid item>
-                <TextField
-                  margin="normal"
-                  fullWidth
-                  size="small"
-                  placeholder="رمز عبور"
-                  name="password"
-                  variant="outlined"
-                  id="password"
-                  value={formik.values.password}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  error={
-                    formik.touched.password && Boolean(formik.errors.password)
-                  }
-                  helperText={formik.touched.password && formik.errors.password}
-                  sx={{
-                    width: { xs: "90%", sm: "77%" },
-                    bgcolor: "whitesmoke",
-                    color: "black",
-                  }}
-                />
-              </Grid>
-              <Grid item>
-                <TextField
-                  margin="normal"
-                  fullWidth
-                  size="small"
-                  placeholder=" تکرار رمز عبور "
-                  name="repeatPassword"
-                  variant="outlined"
-                  id="repeatPassword"
-                  value={formik.values.repeatPassword}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  error={
-                    formik.touched.repeatPassword &&
-                    Boolean(formik.errors.repeatPassword)
-                  }
-                  helperText={
-                    formik.touched.repeatPassword &&
-                    formik.errors.repeatPassword
-                  }
-                  sx={{
-                    width: { xs: "90%", sm: "77%" },
-                    bgcolor: "whitesmoke",
-                    color: "black",
-                  }}
-                />
-              </Grid>
-              <Grid>
-                <Button variant="contained" sx={{ marginY: "30px" }}>
-                  عضویت
-                </Button>
-              </Grid>
-            </Grid>
-          </CardContent>
-        </form>
-      </Container>
+                ثبت نام
+                {console.log("button clicked")}
+              </Button>{" "}
+              <Button
+                style={{ marginTop: "30px", marginRight: "40px" }}
+                type="submit"
+                variant="contained"
+                color="primary"
+                // onClick={navigate("/")}
+              >
+                {" "}
+                پشیمون شدم
+              </Button>
+            </div>
+          </form>
+        </Grid>
+      </Grid>
     </>
   );
 };
 
-export default SignIn;
+export default FormList;
