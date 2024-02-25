1cd96610371bb5b0a27ea029e0be79758f6b9bf9
mohammad parsapasand <mohmd.pars4@gmail.com>
2024-02-25 04:38:22 -0800

    style(Login.jsx): make some change in style

--- src/Component/header/headerTabs/Login.jsx
+++ src/Component/header/headerTabs/Login.jsx
@@ -1,12 +1,12 @@
 import { Card, CardContent, Grid, TextField, Button } from "@mui/material";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 import { useFormik } from "formik";
 import validationSchema from "../../../validations/Schema";
 
 const Login = () => {
   const formik = useFormik({
     initialValues: {
-      email: "",
+      fullname: "",
       password: "",
     },
     validationSchema: validationSchema,
@@ -22,54 +22,67 @@ const Login = () => {
         alignItems="center"
         style={{ minHeight: "80vh" }}
       >
-        <Grid item xs={12} sm={8} md={6} lg={6}>
+        <Grid item xs={10} sm={8} md={6} lg={6}>
           <Card
-            sx={{ height: "70vh", display: "flex", flexDirection: "column" }}
-            style={{ backgroundColor: "gray" }}
+          sx={{marginTop:"10px" , borderRadius:"10px"}}
+            style={{boxShadow:"0 0 5px 5px gray"}}
           >
             <h1 style={{ textAlign: "center", color: "white" }}>فرم ورود</h1>
             <CardContent sx={{ display: "flex", flexDirection: "column" }}>
-              <TextField
-                id="standard-basic"
-                label="ایمیل"
-                name="email"
-                variant="standard"
-                sx={{ marginTop: "10px" }}
-                value={formik.values.email}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                error={formik.touched.email && Boolean(formik.errors.email)}
-                helperText={formik.touched.email && formik.errors.email}
-                InputLabelProps={{ dir: "rtl" }}
-              />
-              <TextField
-                id="standard-basic"
-                label="رمز عبور"
-                name="password"
-                variant="standard"
-                sx={{ marginTop: "20px" }}
-                value={formik.values.password}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                error={formik.touched.password && Boolean(formik.errors.password)}
-                helperText={formik.touched.password && formik.errors.password}
-                InputLabelProps={{ dir: "rtl" }}
-              />
-              <Button
-                style={{
-                  backgroundColor: "white",
-                  color: "black",
-                  marginTop: "50px",
-                  height: "40px",
-                  fontSize: "20px",
-                }}
-              >
-                ورود
-              </Button>
-              <div display="flex" style={{marginTop:"35px"}}>
-                <span style={{ fontSize: "17px" }}>عضو نیستی؟</span>
-                <Link to="../SignIn" style={{textDecoration:"none" , marginRight:"10px"}}>ثبت نام</Link>
-              </div>
+              <form action="">
+                <div  style={{ height: "55vh", display: "flex", flexDirection: "column"}}>
+                  <TextField
+                    id="standard-basic"
+                    label="نام و نام خانوادگی"
+                    name="fullname"
+                    variant="outlined"
+                    sx={{ marginTop: "10px"}}
+                    value={formik.values.fullname}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    error={formik.touched.fullname && Boolean(formik.errors.fullname)}
+                    helperText={formik.touched.fullname && formik.errors.fullname}
+                    InputLabelProps={{ dir: "rtl" }}
+                  />
+                  <TextField
+                    id="standard-basic"
+                    label="رمز عبور"
+                    name="password"
+                    variant="outlined"
+                    sx={{ marginTop: "20px" }}
+                    value={formik.values.password}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    error={
+                      formik.touched.password && Boolean(formik.errors.password)
+                    }
+                    helperText={
+                      formik.touched.password && formik.errors.password
+                    }
+                    InputLabelProps={{ dir: "rtl" }}
+                  />
+                  <Button
+                    style={{
+                      backgroundColor: "gray",
+                      color: "white",
+                      marginTop: "50px",
+                      height: "40px",
+                      fontSize: "20px",
+                    }}
+                  >
+                    ورود
+                  </Button>
+                  <div display="flex" style={{ marginTop: "35px" }}>
+                    <span style={{ fontSize: "17px" }}>عضو نیستی؟</span>
+                    <Link
+                      to="../SignIn"
+                      style={{ textDecoration: "none", marginRight: "10px" }}
+                    >
+                      ثبت نام
+                    </Link>
+                  </div>
+                </div>
+              </form>
             </CardContent>
           </Card>
         </Grid>
