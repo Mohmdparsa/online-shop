ce8c98b94d39ccf3c23a869ce4f48cd7120f2336
mohammad parsapasand <mohmd.pars4@gmail.com>
2024-02-26 05:47:29 -0800

    refactor(ProductsPartition.jsx):integrate card data using useSelector and loop through card data in the component

--- src/Component/ProductsPartition.jsx
+++ src/Component/ProductsPartition.jsx
@@ -1,123 +1,63 @@
 import Grid from "@mui/material/Unstable_Grid2";
 import * as React from "react";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Typography from "@mui/material/Typography";
 import { Button, CardActionArea, CardActions } from "@mui/material";
+import { Typography, CardContent, CardMedia, Card } from "@mui/material";
+import { selectCards } from "../features/ProductsCardsSlice";
+import { useSelector } from "react-redux";
 const ProductsPartition = () => {
+  const cards = useSelector(selectCards);
   return (
     <>
-      <Grid container sx={{ mb: 2, mt: 2 , display:"flex"}}>
-        <Grid xs={12} sm={6} md={6} lg={4} >
-              <Card sx={{ maxWidth: 345 , bgcolor:"primary.main" , color:"seconadary.main", ml:8 , mb:4 , mr:8}}>
-          <CardActionArea>
-            <CardMedia
-              component="img"
-              height="140"
-              image="/static/images/cards/contemplative-reptile.jpg"
-              alt="green iguana"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Lizard
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lizards are a widespread group of squamate reptiles, with over
-                6,000 species, ranging across all continents except Antarctica
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-          <CardActions>
-            <Button size="small" color="secondary">
-              Share
-            </Button>
-          </CardActions>
-        </Card>
-        </Grid>
-
-        <Grid xs={12} sm={6} md={6} lg={4}>
-              <Card sx={{ maxWidth: 345 , bgcolor:"red" , ml:8 , mb:4 , mr:8}}>
-          <CardActionArea>
-            <CardMedia
-              component="img"
-              height="140"
-              image="/static/images/cards/contemplative-reptile.jpg"
-              alt="green iguana"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Lizard
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lizards are a widespread group of squamate reptiles, with over
-                6,000 species, ranging across all continents except Antarctica
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-          <CardActions>
-            <Button size="small" color="primary">
-              Share
-            </Button>
-          </CardActions>
-        </Card>
-        </Grid>
-
-        <Grid xs={12} sm={6} md={6} lg={4}>
-              <Card sx={{ maxWidth: 345, bgcolor:"red" , ml:8 , mb:4 , mr:8}}>
-          <CardActionArea>
-            <CardMedia
-              component="img"
-              height="140"
-              image="/static/images/cards/contemplative-reptile.jpg"
-              alt="green iguana"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Lizard
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lizards are a widespread group of squamate reptiles, with over
-                6,000 species, ranging across all continents except Antarctica
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-          <CardActions>
-            <Button size="small" color="primary">
-              Share
-            </Button>
-          </CardActions>
-        </Card>
-        </Grid>
-        <Grid xs={12} sm={8} md={6} lg={4}>
-              <Card sx={{ maxWidth: 345 , bgcolor:"red" , ml:8 , mb:4 , mr:8}}>
-          <CardActionArea>
-            <CardMedia
-              component="img"
-              height="140"
-              image="/static/images/cards/contemplative-reptile.jpg"
-              alt="green iguana"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Lizard
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lizards are a widespread group of squamate reptiles, with over
-                6,000 species, ranging across all continents except Antarctica
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-          <CardActions>
-            <Button size="small" color="primary">
-              Share
-            </Button>
-          </CardActions>
-        </Card>
-        </Grid>
-      
-      
-    
+      <Grid container sx={{ mb: 2, mt: 2 }} spacing={1}>
+        {cards?.map((card) => (
+          <Grid item xs={12} sm={6} md={6} lg={4}>
+            <Card
+              key={card.id}
+              sx={{
+                maxWidth: 345,
+                bgcolor: "primary.main",
+                color: "seconadary.main",
+                ml: 8,
+                mb: 4,
+                mr: 8,
+              }}
+              xs={12}
+              sm={6}
+              md={8}
+              lg={12}
+            >
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  height="140"
+                  sx={{
+                    width: {
+                      xl: "400px",
+                      lg: "300px",
+                      md: "150px",
+                      sm: "150px",
+                    },
+                  }}
+                  image="/static/images/cards/contemplative-reptile.jpg"
+                  alt="green iguana"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {card.title}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {card.content}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Button size="small" color="secondary">
+                  Share
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        ))}
       </Grid>
     </>
   );
