19bae98eea5dcc98e89f3bc1126bd0938946a5a5
mohammad parsapasand <mohmd.pars4@gmail.com>
2024-02-26 05:42:29 -0800

    feat(store.js): add cards field to store reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import {configureStore} from "@reduxjs/toolkit"
 import formDataSlice from "../features/FormSlice"
+import CardsReducer from "../features/ProductsCardsSlice"
 
 
 const store = configureStore({
     reducer:{
-        formData : formDataSlice
+        formData : formDataSlice,
+        cards : CardsReducer
 
     }
 })
