ac855f41cae6a4a673d7d74c35119359ca786791
mohammad parsapasand <mohmd.pars4@gmail.com>
2024-02-27 04:55:39 -0800

    feat(FooterContainer.jsx): create FooterContainer but without content yet

---
+++ src/Component/footer/FooterContainer.jsx
@@ -0,0 +1,19 @@
+import { Grid } from "@mui/material";
+
+const FooterContainer = () => {
+  return (
+    <div>
+      <Grid
+        container
+        sx={{
+          backgroundColor: "secondary.main",
+          height: "35vh",
+        }}
+      >
+        <h1>hello</h1>
+      </Grid>
+    </div>
+  );
+};
+
+export default FooterContainer;
