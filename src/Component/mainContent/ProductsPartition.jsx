4f267e13e8cb3c9aa601d5d7adb67c35d985b2fc
mohammad parsapasand <mohmd.pars4@gmail.com>
2024-02-27 06:53:05 -0800

    refactor(ProductsPartition.jsx): add link for navigation and remove content

--- src/Component/mainContent/ProductsPartition.jsx
+++ src/Component/mainContent/ProductsPartition.jsx
@@ -4,6 +4,7 @@ import { Button, CardActionArea, CardActions } from "@mui/material";
 import { Typography, CardContent, CardMedia, Card } from "@mui/material";
 import { selectCards } from "../../features/ProductsCardsSlice";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 const ProductsPartition = () => {
   const cards = useSelector(selectCards);
   return (
@@ -11,51 +12,50 @@ const ProductsPartition = () => {
       <Grid container sx={{ mb: 2, mt: 2 }} spacing={1}>
         {cards?.map((card) => (
           <Grid item xs={12} sm={6} md={6} lg={4}>
-            <Card
-              key={card.id}
-              sx={{
-                maxWidth: 345,
-                bgcolor: "primary.main",
-                color: "seconadary.main",
-                ml: 8,
-                mb: 4,
-                mr: 8,
-              }}
-              xs={12}
-              sm={6}
-              md={8}
-              lg={12}
-            >
-              <CardActionArea>
-                <CardMedia
-                  component="img"
-                  height="140"
-                  sx={{
-                    width: {
-                      xl: "400px",
-                      lg: "300px",
-                      md: "150px",
-                      sm: "150px",
-                    },
-                  }}
-                  image="/static/images/cards/contemplative-reptile.jpg"
-                  alt="green iguana"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                    {card.title}
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    {card.content}
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-              <CardActions>
-                <Button size="small" color="secondary">
-                  Share
-                </Button>
-              </CardActions>
-            </Card>
+            <Link to={`/${card.title}`} key={card.id} style={{textDecoration:"none"}}>
+              <Card
+                key={card.id}
+                sx={{
+                  maxWidth: 345,
+                  bgcolor: "primary.main",
+                  color: "seconadary.main",
+                  ml: 8,
+                  mb: 4,
+                  mr: 8,
+                }}
+                xs={12}
+                sm={6}
+                md={8}
+                lg={12}
+              >
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    height="140"
+                    sx={{
+                      width: {
+                        xl: "400px",
+                        lg: "300px",
+                        md: "150px",
+                        sm: "150px",
+                      },
+                    }}
+                    image="/static/images/cards/contemplative-reptile.jpg"
+                    alt="green iguana"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      {card.title}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Button size="small" color="secondary" sx={{fontSize:"16px" , color:"blue"}}>
+                    نمایش
+                  </Button>
+                </CardActions>
+              </Card>
+            </Link>
           </Grid>
         ))}
       </Grid>
