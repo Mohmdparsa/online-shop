e20b1b79d852c3b3303d1332d78f75e544eb76b9
mohammad parsapasand <mohmd.pars4@gmail.com>
2024-02-27 07:14:35 -0800

    refactor(ProductsCardsSlice.js): add identifier to each object

--- src/features/ProductsCardsSlice.js
+++ src/features/ProductsCardsSlice.js
@@ -2,14 +2,14 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   cards: [
-    { id: 1, title: "کالای دیجیتال" },
-    { id: 2, title: "خانه و آشپزخانه" },
-    { id: 3, title: "خدمات" },
-    { id: 4, title: "وسایل شخصی" },
-    { id: 5, title: "سرگرمی و فراغت" },
-    { id: 6, title: "اجتماعی" },
-    { id: 7, title: "تجهیزات  و صنعتی" },
-    { id: 8, title: "وسایل نقلیه" },
+    { id: 1, title: "کالای دیجیتال" , identifier:"card1"},
+    { id: 2, title: "خانه و آشپزخانه"  , identifier:"card2" },
+    { id: 3, title: "خدمات"  , identifier:"card3" },
+    { id: 4, title: "وسایل شخصی"  , identifier:"card4"},
+    { id: 5, title: "سرگرمی و فراغت"  , identifier:"card5" },
+    { id: 6, title: "اجتماعی"  , identifier:"card6"},
+    { id: 7, title: "تجهیزات  و صنعتی"  , identifier:"card7"},
+    { id: 8, title: "وسایل نقلیه"  , identifier:"card8"},
   ],
 };
 const ProductCardsSlice = createSlice({
