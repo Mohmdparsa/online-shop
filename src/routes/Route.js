5376dea976639c6c81278140960ab769667d6cfb
mohammad parsapasand <mohmd.pars4@gmail.com>
2024-02-29 01:56:13 -0800

    feat(Route.js): add DigitalProducts path

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -1,36 +1,39 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 import App from "../App";
-import SignIn from "../Component/header/headerTabs/SignIn"
+import SignIn from "../Component/header/headerTabs/SignIn";
 import Login from "../Component/header/headerTabs/Login";
 import ShoppingCart from "../Component/header/headerTabs/ShoppingCart";
-
+import DigitalProducts from "../pages/DigitalProducts";
 
 const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <MainLayout />,
-      errorElement: <h3>چیزی پیدا نکردیم متاسفانه...</h3>,
-      children: [
-        {
-          path: "/",
-          element: <App />,
-        },
-        {
-          path:"/SignIn",
-          element:<SignIn/>
-        },
-        {
-          path:"/Login",
-          element:<Login/>
-        },
-        {
-          path:"/ShoppingCart",
-          element:<ShoppingCart/>
-        }
+  {
+    path: "/",
+    element: <MainLayout />,
+    errorElement: <h3>چیزی پیدا نکردیم متاسفانه...</h3>,
+    children: [
+      {
+        path: "/",
+        element: <App />,
+      },
+      {
+        path: "/SignIn",
+        element: <SignIn />,
+      },
+      {
+        path: "/Login",
+        element: <Login />,
+      },
+      {
+        path: "/ShoppingCart",
+        element: <ShoppingCart />,
+      },
+      {
+        path: "Card1",
+        element: <DigitalProducts />,
+      },
+    ],
+  },
+]);
 
-      ],
-    },
-  ]);
-
-  export default router
+export default router;
