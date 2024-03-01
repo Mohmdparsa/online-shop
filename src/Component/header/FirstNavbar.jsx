ec3b408026dea2b2c08da79b1ea20c88f6d16947
mohammad parsapasand <mohmd.pars4@gmail.com>
2024-03-01 10:52:41 -0800

    refactor(FirstNavbar.jsx): make SignIn and login button display none in xs size

--- src/Component/header/FirstNavbar.jsx
+++ src/Component/header/FirstNavbar.jsx
@@ -9,14 +9,13 @@ import {
   Button,
   Card,
   IconButton,
+  Grid,
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import SearchIcon from "@mui/icons-material/Search";
 import LoginRoundedIcon from "@mui/icons-material/LoginRounded";
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
-import { Link } from 'react-router-dom';
-
-
+import { Link } from "react-router-dom";
 
 const Search = styled("div")(({ theme }) => ({
   position: "relative",
@@ -49,7 +48,6 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
   width: "100%",
   "& .MuiInputBase-input": {
     padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
     transition: theme.transitions.create("width"),
     [theme.breakpoints.up("sm")]: {
@@ -92,38 +90,49 @@ const FirstHeader = () => {
               inputProps={{ "aria-label": "search" }}
             />
           </Search>
-          <Card
-            sx={{
-              backgroundColor: "primary.main",
-              border: "1px solid black",
-              marginLeft: "20px",
-              width: "160px",
-              display: "flex",
-            }}
-          >
-            <Button variant="text" sx={{ color: "blue", fontSize: "15px" }}>
-              <Link to="Login" style={{textDecoration: 'none'}}>
-              ورود
+          <Grid contaner sx={{display:{xs:"none" , sm:"block"}}}>
+            {" "}
+            <Grid item sx={{display:"flex"}}>
+              <Card
+                sx={{
+                  backgroundColor: "primary.main",
+                  border: "1px solid black",
+                  marginLeft: "20px",
+                  width: "160px",
+                  display: "flex",
+                }}
+              >
+                <Button variant="text" sx={{ color: "blue", fontSize: "15px" }}>
+                  <Link to="Login" style={{ textDecoration: "none" }}>
+                    ورود
+                  </Link>
+                </Button>{" "}
+                <span style={{ marginTop: "10px" }}>|</span>
+                <Button
+                  variant="text"
+                  sx={{ color: "blue", fontSize: "15px", mt: "3px" }}
+                >
+                  <Link to="SignIn" style={{ textDecoration: "none" }}>
+                    ثبت نام
+                  </Link>
+                </Button>
+                <LoginRoundedIcon
+                  sx={{ fontSize: "17px", marginTop: "13px" }}
+                />
+              </Card>
+              <span style={{ marginRight: "20px" , marginTop:"12px" }}>|</span>
+              <Link to="ShoppingCart">
+                <IconButton
+                  color="secondary.main"
+                  sx={{ marginLeft: "20px" , marginTop:"5px" }}
+                  aria-label="add to shopping cart"
+                  size="small"
+                >
+                  <AddShoppingCartIcon />
+                </IconButton>
               </Link>
-              
-            </Button>{" "}
-            <span style={{ marginTop: "10px" }}>|</span>
-            <Button
-              variant="text"
-              sx={{ color: "blue", fontSize: "15px", mt: "3px" }}
-            >
-              <Link to="SignIn" style={{textDecoration: 'none'}}>
-              ثبت نام 
-              </Link>
-            </Button>
-            <LoginRoundedIcon sx={{ fontSize: "17px", marginTop: "13px" }} />
-          </Card>
-          <span style={{ marginRight: "20px" }}>|</span>
-        <Link to="ShoppingCart">
-           <IconButton color="secondary.main" sx={{marginLeft:"20px"}} aria-label="add to shopping cart" size="small">
-            <AddShoppingCartIcon />                
-          </IconButton>
-        </Link>
+            </Grid>
+          </Grid>
         </Toolbar>
       </AppBar>
     </Box>
