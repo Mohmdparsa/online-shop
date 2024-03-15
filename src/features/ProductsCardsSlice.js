196c01d2f31f50eb6c4f98e26cda260f6baf663e
mohammad parsapasand <mohmd.pars4@gmail.com>
2024-03-15 15:31:10 -0700

    feat(ProductsCardsSlice.js):implement asynchronous data fetching cards data  from rest api

--- src/features/ProductsCardsSlice.js
+++ src/features/ProductsCardsSlice.js
@@ -1,25 +1,37 @@
-import { createSlice } from "@reduxjs/toolkit";
-import DigitalProducts from "../assets/MainPageImage/DigitalProducts.jpg"
-import entertainment from "../assets/MainPageImage/entertainmentProducts.jpg"
-import houseProducts from "../assets/MainPageImage/houseProducts.png"
-import industrialProduct from "../assets/MainPageImage/industerialProduct.jpg"
-import transportProducts from "../assets/MainPageImage/transportProducts.jpg"
-import sportsProducts from "../assets/MainPageImage/sportsProducts.jpg"
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { getAllCards } from "../Services/CardsServices";
+
 const initialState = {
-  cards: [
-    { id: 1, title: "کالای دیجیتال" , identifier:"card1" , image: DigitalProducts},
-    { id: 2, title: "خانه و آشپزخانه"  , identifier:"card2" ,image: houseProducts},
-    { id: 3, title: "سرگرمی و فراغت"  , identifier:"card3" , image: entertainment},
-    { id: 4, title: "تجهیزات  و صنعتی"  , identifier:"card4" , image: industrialProduct},
-    { id: 5, title: "وسایل نقلیه"  , identifier:"card5" , image: transportProducts},
-    { id: 6, title: "وسایل ورزشی"  , identifier:"card6" , image: sportsProducts}
-  ],
+  cards: [],
+  status: "idle",
+  error: null,
 };
+
+export const fetchCards = createAsyncThunk("/cards/fetchCards", async () => {
+  const response = await getAllCards();
+  return response.data;
+});
+
 const ProductCardsSlice = createSlice({
   name: "cards",
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchCards.pending, (state, action) => {
+        state.status = "pending";
+      })
+      .addCase(fetchCards.fulfilled, (state, action) => {
+        state.status = "completed";
+        state.cards = action.payload;
+      })
+      .addCase(fetchCards.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
+      });
+  },
 });
 
 export default ProductCardsSlice.reducer;
 export const selectCards = (state) => state.cards.cards;
+export const selectCardsStatus = (state) => state.cards.status;
