07d0ab02fb98026c313d4b0efbf27842c4311e92
mohammad parsapasand <mohmd.pars4@gmail.com>
2024-03-17 11:03:06 -0700

    refactor(DigitalProducts.jsx): remove unused component and import DigitalProductsSidebar.jsx

--- src/pages/DigitalProducts/DigitalProducts.jsx
+++ src/pages/DigitalProducts/DigitalProducts.jsx
@@ -1,9 +1,9 @@
-import * as React from "react";
+
 import PropTypes from "prop-types";
-import { Box, Typography, Tab, Tabs } from "@mui/material";
-import { useDispatch } from "react-redux";
-import { setSelectedCategory } from "../../features/DigitalProductsSlice";
+import { Box, Typography} from "@mui/material";
+
 import DigitalProductsCardContent from "./DigitalProductsCardContent";
+import DigitalProductsSidebar from "./DigitalProductsSidebar"
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -31,23 +31,10 @@ TabPanel.propTypes = {
   value: PropTypes.number.isRequired,
 };
 
-function a11yProps(index) {
-  return {
-    id: `vertical-tab-${index}`,
-    "aria-controls": `vertical-tabpanel-${index}`,
-  };
-}
+
 
 const DigitalProducts = () => {
-  const [value, setValue] = React.useState(0);
-  const dispatch = useDispatch();
-  const handleCategoryClick = (category) => {
-    dispatch(setSelectedCategory(category));
-  };
 
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
 
   return (
     <Box
@@ -58,111 +45,7 @@ const DigitalProducts = () => {
         height: "100vh",
       }}
     >
-      <Tabs
-        orientation="vertical"
-        variant="scrollable"
-        value={value}
-        onChange={handleChange}
-        aria-label="Vertical tabs example"
-        sx={{
-          borderRight: 1,
-          borderColor: "divider",
-          backgroundColor: "secondary.main",
-          width: {
-            // xs: "150px",
-            sm: "300px",
-            md: "270px",
-            lg: "240px",
-            xl: "370px",
-          },
-          display:{
-            xs:"none",
-            sm:"block"
-          }
-        }}
-      >
-        <Tab
-          label="موبایل"
-          {...a11yProps(0)}
-          onClick={() => handleCategoryClick("products")}
-          sx={{
-            backgroundColor: "gray",
-            width: "170px",
-            mx: "17px",
-            borderRadius: "5px",
-            my: "5px",
-          }}
-        />
-        <Tab
-          label="لپ تاپ"
-          {...a11yProps(1)}
-          onClick={() => handleCategoryClick("cars")}
-          sx={{
-            backgroundColor: "gray",
-            width: "170px",
-            mx: "17px",
-            borderRadius: "5px",
-            my: "5px",
-          }}
-        />
-        <Tab
-          label="ساعت هوشمند"
-          {...a11yProps(2)}
-          onClick={() => handleCategoryClick("foods")}
-          sx={{
-            backgroundColor: "gray",
-            width: "170px",
-            mx: "17px",
-            borderRadius: "5px",
-            my: "5px",
-          }}
-        />
-        <Tab
-          label="کیبورد"
-          {...a11yProps(3)}
-          sx={{
-            backgroundColor: "gray",
-            width: "170px",
-            mx: "17px",
-            borderRadius: "5px",
-            my: "5px",
-          }}
-        />
-        {/* <Tab
-          label=""
-          {...a11yProps(4)}
-          sx={{
-            backgroundColor: "gray",
-            width: "170px",
-            mx: "17px",
-            borderRadius: "5px",
-            my: "5px",
-          }}
-        />
-        <Tab
-          label="Item Six"
-          {...a11yProps(5)}
-          sx={{
-            backgroundColor: "gray",
-            width: "170px",
-            mx: "17px",
-            borderRadius: "5px",
-            my: "5px",
-          }}
-        />
-        <Tab
-          label="Item Seven"
-          {...a11yProps(6)}
-          sx={{
-            backgroundColor: "gray",
-            width: "170px",
-            mx: "17px",
-            borderRadius: "5px",
-            my: "5px",
-          }}
-        /> */}
-      </Tabs>
-
+   <DigitalProductsSidebar/>
       <DigitalProductsCardContent />
     </Box>
   );
